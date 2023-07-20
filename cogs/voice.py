b3bc7d4eb357b24f09f37fb342eb8948dda91e00
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-07-20 17:05:04 +0200

    Initial commit

---
+++ cogs/voice.py
@@ -0,0 +1,35 @@
+import disnake
+from disnake.ext import commands, tasks
+from env import *
+
+class Voice(commands.Cog):
+
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        print("Cog Voice is geladen!")
+
+
+    # Commands
+    @commands.default_member_permissions(moderate_members=True)
+    @commands.slash_command()
+    async def voice(self, inter):
+        pass
+
+
+    @voice.sub_command(description="versleep een lid naar een ander voice kanaal")
+    async def versleep(self, inter, gebruiker: disnake.Member, kanaal=commands.Param(choices={"Lounge": "lounge", "Bezig": "bezig", "AFK": "afk"})):
+        if kanaal == "lounge":
+            channel = self.bot.get_channel(Env.VC_LOUNGE_ID)
+        elif kanaal == "bezig":
+            channel = self.bot.get_channel(Env.VC_BEZIG_ID)
+        elif kanaal == "afk":
+            channel = self.bot.get_channel(Env.VC_AFK_ID)
+
+        await gebruiker.edit(voice_channel=channel)
+
+        await inter.response.send_message(f"Gebruiker `{gebruiker.name}` is naar `{channel.name}` verplaatst",ephemeral=True)
+
+        
+def setup(bot: commands.Bot):
+    bot.add_cog(Voice(bot))
---
+++ main.py
@@ -0,0 +1,20 @@
+import disnake
+from disnake.ext import commands
+from env import *
+
+
+intents = disnake.Intents.all()
+bot = commands.Bot(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.listening , name="jou"))
+    print("De bot is ready to rock!")
+
+
+bot.load_extension("cogs.voice")
+
+
+if __name__ == "__main__":
+    bot.run(Env.TOKEN)
